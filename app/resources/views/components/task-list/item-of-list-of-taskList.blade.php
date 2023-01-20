cc3ee2082bda9e53ffeb20266c855c2041923142
ShevaWall <alexa7454@gmail.com>
2023-01-20 11:24:15 +0300

    paste # in href

--- app/resources/views/components/task-list/item-of-list-of-taskList.blade.php
+++ app/resources/views/components/task-list/item-of-list-of-taskList.blade.php
@@ -1,4 +1,4 @@
-<a href="/test" class="list-group-item list-group-item-action" aria-current="true">
+<a href="#" class="list-group-item list-group-item-action" aria-current="true" onclick="loadTaskListItems({{$task_list->id}})">
     <span id="taskList_name">{{$task_list->name}}</span>
     <span class="btn-close" onclick="deleteTaskList({{$task_list->id}}, this)"></span>
     <span class="btn-pencil" onclick="showEditForm(this, {{$task_list->id}});" data-bs-toggle="modal" data-bs-target="#editTaskList"></span>
