54124bbbdaed70803fb86df88e878db332a5443d
ShevaWall <alexa7454@gmail.com>
2023-01-21 02:00:38 +0300

    change component. now merge attributes

--- app/resources/views/components/spinner/danger.blade.php
+++ app/resources/views/components/spinner/danger.blade.php
@@ -1,3 +1,4 @@
-<div class="spinner-grow text-danger" role="status">
+<div {!! $attributes->merge(['class' => 'spinner-grow text-danger']) !!}
+    role="status">
     <span class="visually-hidden">Загрузка...</span>
 </div>
