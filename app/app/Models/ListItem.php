0ee2c5c011054d090a489e9eea7d9bdd36e96caf
ShevaWall <alexa7454@gmail.com>
2023-01-20 11:23:58 +0300

    add fillable

--- app/app/Models/ListItem.php
+++ app/app/Models/ListItem.php
@@ -8,4 +8,12 @@ use Illuminate\Database\Eloquent\Model;
 class ListItem extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'name',
+        'description',
+        'file',
+        'completed',
+        'task_list_id',
+    ];
 }
