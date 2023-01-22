9852735dd50997d35e0706dff26a34bd9fe4741f
ShevaWall <alexa7454@gmail.com>
2023-01-23 02:29:18 +0300

    pre finish

--- run.sh
+++ run.sh
@@ -1,10 +1,15 @@
 #!/usr/bin/env sh
 
+composer install
+
 echo "Check DB!"
 while ! php artisan migrate | grep -E "Nothing to migrate|Command Canceled"; do
   echo "Waiting for init db to complete..."
   sleep 2
 done
 echo "DB ready!"
+
+php artisan key:generate
+php artisan storage:link
 
 php-fpm
